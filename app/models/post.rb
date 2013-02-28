15dfda1caca7a4832e5482c48f6929f5e59370c5
jasonblanchard <jas.blanchard@gmail.com>
2013-02-28 16:44:38 -0500

    Added fail_create.js and DRYd up error messages

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
   attr_accessible :body
   has_many :comments, :dependent => :destroy
 
-  validates :body, :length => { :maximum => 140, :message => "Gotta be less than 140 characters!" }
-  validates :body, :presence => { :message => "You've gotta put something..." }
+  validates :body, :length => { :maximum => 140, :message => "must be less than 140 characters!" }
+  validates :body, :presence => { :message => "has to have something..." }
 
   def has_comments?
       comments.length > 0
